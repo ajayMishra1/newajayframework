ea700938a20b05f49231e1f621727fe3b895f8bb
ajay mishra <mishraajay95124@gmail.com>
2020-02-28 23:10:24 +0530

    ajay first

---
+++ Example/Pods/Target Support Files/Pods-newajayframework_Tests/Pods-newajayframework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_newajayframework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_newajayframework_Tests
+@end
---
+++ Example/Pods/Target Support Files/newajayframework/newajayframework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_newajayframework : NSObject
+@end
+@implementation PodsDummy_newajayframework
+@end
